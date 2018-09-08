7176f88e815922dc33bf4772e381f522a64aeb2f
qa <qa.bh@gj>
2018-09-08 20:37:00 +0300

    Default Changelist

--- src/org/itstep/qa/lessons/lesson_six/Task09.java
+++ src/org/itstep/qa/lessons/lesson_six/Task09.java
@@ -3,8 +3,8 @@ package org.itstep.qa.lessons.lesson_six;
 public class Task09 {
     public static void main(String[] args) {
         // проверить является ли введенный символ цифрой
-        Character ch = new Character('A');
-        boolean ch1 = Character.isDigit(ch);
+        Character ch = new Character('A');// массив char
+        boolean ch1 = Character.isDigit(ch);// проверяет, явл. ли символ цифрой
         System.out.println(ch1);
     }
 }
