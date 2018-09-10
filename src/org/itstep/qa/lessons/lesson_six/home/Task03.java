c11a13b9729a8da5812bf22d5ce1b0869d889723
mike <mike@gmail.com>
2018-09-10 13:36:51 +0300

    Default

--- src/org/itstep/qa/lessons/lesson_six/home/Task03.java
+++ src/org/itstep/qa/lessons/lesson_six/home/Task03.java
@@ -1,5 +1,18 @@
 package org.itstep.qa.lessons.lesson_six.home;
 
+import javafx.scene.transform.Scale;
+
+import java.util.Scanner;
+
 public class Task03 {
     // Даны две строки. Определите, является ли первая строка подстрокой второй строки. (Можно использовать метод contains).
+    public static void main(String[] args) {
+        String str1 = new String("privet");
+        String str2 = new String("privrtu");
+        boolean b = str2.contains(str1);
+        System.out.println(b);
+        if(str2.contains("privet")){
+            System.out.println("Первая строка является подстрокой для второй строки");
+        } else System.out.println("Первая строка не является подстрокой для второй строки");
+    }
 }
---
+++ src/org/itstep/qa/lessons/lesson_six/home/Task05.java
@@ -0,0 +1,5 @@
+package org.itstep.qa.lessons.lesson_six.home;
+
+public class Task05 {
+    // Изменить регистр введенного символа, если он был латинской буквой.
+}
