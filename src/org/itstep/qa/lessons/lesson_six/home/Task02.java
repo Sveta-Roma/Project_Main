7264d794a5607f54b2913190cb438dd4a6679681
mike <mike@gmail.com>
2018-09-11 12:31:12 +0300

    updat

--- src/org/itstep/qa/lessons/lesson_six/home/Task02.java
+++ src/org/itstep/qa/lessons/lesson_six/home/Task02.java
@@ -3,13 +3,14 @@ package org.itstep.qa.lessons.lesson_six.home;
 import java.util.Scanner;
 
 public class Task02 {
+    // Создать строку и проинициализировать ее каким-либо значением. Вывесть значение строки на экран.
+    // Присвоить этой же строке значение введенное с клавиатуры и также вывести его на экран
     public static void main(String[] args) {
-        // Создать строку и проинициализировать ее каким-либо знгачениемю. Вывесть значение строки на экран.
-        // Присвоить этой же строке значение введенное с клавиатуры и также вывести его на экран
-
+        System.out.println("Исходный текст:");
         String str = new String ("dfghjb");
         System.out.println(str);
         Scanner sc = new Scanner(System.in);
+        System.out.println("Введите текст:");
         str = sc.nextLine();
         System.out.println(str);
     }
--- src/org/itstep/qa/lessons/lesson_six/home/Task03.java
+++ src/org/itstep/qa/lessons/lesson_six/home/Task03.java
@@ -9,9 +9,18 @@ public class Task03 {
     public static void main(String[] args) {
         String str1 = new String("privet");
         String str2 = new String("privrtu");
+
+        /* ввод с консоли
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Первая строка:");
+        String str1 = sc.nextLine();
+        System.out.println("Вторая строка:");
+        String str2 = sc.nextLine();
+        */
+
         boolean b = str2.contains(str1);
         System.out.println(b);
-        if(str2.contains("privet")){
+        if(str2.contains(str1)){
             System.out.println("Первая строка является подстрокой для второй строки");
         } else System.out.println("Первая строка не является подстрокой для второй строки");
     }
